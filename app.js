6df156ec5afdac9f259ad9a8c4da752b827ae214
jamiejamiebobamie <jmccrory@vt.edu>
2020-07-15 17:33:19 -0700

    added stuff

--- app.js
+++ app.js
@@ -7,29 +7,15 @@ app.use(cookieParser());
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-
 //middleware for JSON data
 const bodyParser = require('body-parser');
-
-// const expressValidator = require('express-validator');
-const {check, validationResult} = require('express-validator/check');
-
-//middleware for "putting" something when you "post" it
-    // the "U" in "CRUD" edit / update.
-const methodOverride = require('method-override');
-
+const expressValidator = require('express-validator');
 // Use Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT || 15000;
-
 // this is deprecated:
 // app.use(expressValidator());
-    // must use check()
-    //https://auth0.com/blog/express-validator-tutorial/
-
-// override with POST having ?_method=DELETE or ?_method=PUT
-app.use(methodOverride('_method'))
 var checkAuth = (req, res, next) => {
   console.log("Checking authentication");
   if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
@@ -41,7 +27,6 @@ var checkAuth = (req, res, next) => {
   }
   next();
 };
-
 app.use(checkAuth);
 const User = require('./models/user.js');
 const users = require('./controllers/users.js')(app);
@@ -49,19 +34,11 @@ const Comment = require('./models/comment.js');
 const comments = require('./controllers/comments.js')(app);
 const Session = require('./models/session.js');
 const sessions = require('./controllers/sessions.js')(app);
-
 app.use(express.static('Public'));
-
 //heroku database.
 mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost/db'), { useNewUrlParser: true });
-
 //views middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.listen(port);
-
-app.get('/', (req, res) => {
-    res.render('index',{});
-})
-
 module.exports = app;
--- controllers/users.js
+++ controllers/users.js
@@ -5,5 +5,74 @@ const Session = require('../models/session');
 const Comment = require('../models/comment');
 
 module.exports = app => {
-    // put our User routes here.
+    app.get('/sign-up', (req, res) => {
+        let landing = false;
+        var currentUser = req.user;
+        if (currentUser) {
+            res.redirect('/');
+        } else {
+            res.render('sign-up', currentUser, landing);
+        }
+    });
+
+    // SIGN UP POST
+    app.post("/sign-up", (req, res) => {
+      // Create User and JWT
+      const user = new User(req.body);
+      user.save().then((user) => {
+          var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+          res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+          res.redirect('/'); // it seems there is a bug here.
+      })                     // new users cannot post after signing up. 
+                             // they need to log in... might not occur when
+                             // integrated with the socket.io part of the app.
+        .catch(err => {
+          console.log(err.message);
+          return res.status(400).send({ err: err });
+        });
+
+    });
+
+     // LOGIN
+    app.post("/login", (req, res) => {
+      const username = req.body.username;
+      const password = req.body.password;
+      // Find this user name
+      User.findOne({ username }, "username password")
+        .then(user => {
+          if (!user) {
+            // User not found
+            return res.status(401).send({ message: "Wrong Username or Password" });
+          }
+          // Check the password
+          user.comparePassword(password, (err, isMatch) => {
+            if (!isMatch) {
+              // Password does not match
+              return res.status(401).send({ message: "Wrong Username or password" });
+            }
+            // Create a token
+            const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+              expiresIn: "60 days"
+            });
+            // Set a cookie and redirect to root
+            res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+            res.redirect("/");
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+
+    // LOGOUT
+    app.get('/logout', (req, res) => {
+      res.clearCookie('nToken');
+      res.redirect('/');
+    });
+
+    // LOGIN FORM
+     app.get('/login', (req, res) => {
+         let landing = false;
+       res.render('login', { landing });
+     });
 };
